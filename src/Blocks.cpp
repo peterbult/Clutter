64dd843c6c5bbcf8cd69961e6dbc4336511ff2da
Peter Bult <p.m.bult@gmail.com>
2016-01-24 13:38:29 +0100

    Add option block stucts
    Command line options are collected in 'blocks', which are simple data
    structures. Two block types are added:
    
    - A HelpBlock: containing the model command line option as defined by
      the user
    
    - A CommandBlock: a realized command line option as parsed by Clutter.

---
+++ src/Blocks.cpp
@@ -0,0 +1,21 @@
+// 
+// Blocks.cpp
+// Clutter
+//
+// Created by PM Bult on 24-01-2016
+// Copyright (c) P.M. Bult. All rights reserved.
+//
+// Version 1.0
+//
+
+#include <iostream>
+
+#include "Option.h"
+
+namespace Clutter {
+    void HelpBlock::print()
+    {
+        printf( "%s %s %s\n", mTag, mLabel, mHelp );
+    }
+}
+
---
+++ src/Blocks.h
@@ -0,0 +1,46 @@
+// 
+// Blocks.h
+// Clutter
+//
+// Created by PM Bult on 24-01-2016
+// Copyright (c) P.M. Bult. All rights reserved.
+//
+// Version 1.0
+//
+
+#pragma once
+
+namespace Clutter {
+    struct CommandBlock {
+        // Internal Alias
+        typedef std::string string;
+        typedef std::vector vector;
+
+        // Constructor
+        CommandBlock()=default;
+        CommandBlock( vector<string> _values ) : values(_values) { }
+    
+        // Data members
+        bool            processed = false;  // A state flag
+
+        vector<string>  values;             // The list of values
+    };
+
+    struct HelpBlock {
+        // Internal Alias
+        typedef std::string string;
+
+        // Constructor
+        HelpBlock( string _tag, string _label, string _help )
+            : tag( _tag ), label( _label ), help( _help ) { }
+
+        // Output function
+        void print();
+
+        // Data members
+        string tag;       // The short 'tag' identifier
+        string label;     // The long 'label' identifier
+        string help;      // The help message
+    };
+}
+
